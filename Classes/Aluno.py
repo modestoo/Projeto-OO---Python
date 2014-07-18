c5cfcfe74f79d0a4ac8f6998537dc33fb0fe3878
Augusto Modesto <augusto.modesto@live.com>
2014-07-18 18:23:43 -0300

    Alerações na classe Aluno

--- Classes/Aluno.py
+++ Classes/Aluno.py
@@ -1,6 +1,8 @@
-class Aluno:
+class Aluno(Imprimivel):
     "Classe que representa os alunos da escola"
 
+    ###### Atributos de instância e classe
+    
     __matricula = None
     __nome = None
     __dataNascimento = None
@@ -9,10 +11,22 @@ class Aluno:
     __serie = None
     quantidadeAlunosMatriculados = 0
 
-    def __init__(self, matricula, nome, endereco):
-        self.__matricula = matricula
-        self.__nome = nome
-        self.__endereco = endereco
+
+    ###### (Des)Contrutores e Métodos de Negócio
+    
+    def __init__(self):
+        print ("MATRÍCULA DO ALUNO - Entre com as informações a seguir")
+        matricula = int(input("Matrícula do aluno (6 número):"))#Adicionar restrição de 6 letras com a função len (length)
+        nome = input("Nome do aluno:")
+        dataNascimento = input("Data de Nascimento do aluno(__/__/____):")
+        endereco = input("Endereco do aluno:")
+        email = input("Email do aluno:")
+        serie = input("Serie do aluno (_ª Ano):")
+        self.matricularAluno(matricula, nome,
+                             dataNascimento, endereco, email, serie)
+
+    def __del__(self):
+        Aluno.quantidadeAlunosMatriculados -= 1
 
     def matricularAluno(self, matricula, nome,
                         dataNascimento, endereco, email, serie):
@@ -22,8 +36,45 @@ class Aluno:
         self.__endereco = endereco
         self.__email = email
         self.__serie = serie
-        quantidadeAlunosMatriculados += 1
-        
+        Aluno.quantidadeAlunosMatriculados += 1
 
-
+    
+            
+    ###### Acessores e Modificadores
         
+    def getMatricula(self):
+        return self.__matricula
+
+    def setMatricula(self, matricula):
+        self.__matricula = matricula
+
+    def getNome(self):
+        return self.__nome
+
+    def setNome(self, nome):
+        self.__nome = nome
+
+    def getDataNascimento(self):
+        return self.__dataNascimento
+
+    def setDataNascimento(self, DN):
+        self.__dataNascimentoa = DN
+
+    def getEndereco(self):
+        return self.__endereco
+
+    def setEndereco(self, endereco):
+        self._endereco = endereco
+
+    def getEmail(self):
+        return self.__email
+
+    def setEmail(self, email):
+        self._email = email
+
+    def getSerie(self):
+        return self.__serie
+
+    def setSerie(self, serie):
+        self._serie = serie
+
