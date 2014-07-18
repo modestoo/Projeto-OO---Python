3a1da734ff1b3df45b2710b8c0533548da720f7f
Augusto Modesto <augusto.modesto@live.com>
2014-07-18 14:18:34 -0300

    Alterado alguns erros de sintaxe na classe

--- Classes/Aluno.py
+++ Classes/Aluno.py
@@ -1,27 +1,27 @@
 class Aluno:
     "Classe que representa os alunos da escola"
 
-    __matricula_ = 0
-    __nome_ = ""
-    __dataNascimento_ = ""
-    __endereco_ = ""
-    __email_ = ""
-    __serie_ = ""
+    __matricula = None
+    __nome = None
+    __dataNascimento = None
+    __endereco = None
+    __email = None
+    __serie = None
     quantidadeAlunosMatriculados = 0
 
     def __init__(self, matricula, nome, endereco):
-        self.matricula = matricula
-        self.nome = nome
-        self.endereco = endereco
+        self.__matricula = matricula
+        self.__nome = nome
+        self.__endereco = endereco
 
     def matricularAluno(self, matricula, nome,
                         dataNascimento, endereco, email, serie):
-        self.matricula = matricula
-        self.nome = nome
-        self.dataNascimento
-        self.endereco = endereco
-        self.email = email
-        self.serie = serie
+        self.__matricula = matricula
+        self.__nome = nome
+        self.__dataNascimento
+        self.__endereco = endereco
+        self.__email = email
+        self.__serie = serie
         quantidadeAlunosMatriculados += 1
         
 
